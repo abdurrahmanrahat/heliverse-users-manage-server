68bfc461937590227464f44739b5bc64cb07a16c
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2023-11-19 10:04:21 +0600

    get users data from db

--- server.js
+++ server.js
@@ -36,8 +36,11 @@ async function run() {
         const usersCollection = client.db('heliverse-task').collection('users');
 
 
-
-
+        // get user data with email specific
+        app.get('/users', async (req, res) => {
+            const result = await usersCollection.find().toArray();
+            res.send(result);
+        })
 
 
         // Send a ping to confirm a successful connection
